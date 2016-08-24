c6896e76871a56b0b284e193ae045fcc447d9cd5
Lucas Nguyen <lucas.nguyen123@gmail.com>
2016-08-24 15:05:06 +0800

    AI WORKS NOW YAY

--- SP1Framework/AI.cpp
+++ SP1Framework/AI.cpp
@@ -3,20 +3,24 @@ EnemyChar Enemy;
 extern SGameChar g_sChar;
 extern char Map[20][100][100];
 extern int LevelSelection;
-extern double g_dBounceTime;
 extern double g_dElapsedTime;
+double enemyMoveTime = 0.0;
 bool detected = false;
 int randomMovement;
 
 void AiMovement()
 {
 	detected = false;
-	if ((Enemy.m_cLocation.X == g_sChar.m_cLocation.X) && (((Enemy.m_cLocation.Y > g_sChar.m_cLocation.Y) && (Enemy.m_cLocation.Y - g_sChar.m_cLocation.Y <= 5)) || ((g_sChar.m_cLocation.Y > Enemy.m_cLocation.Y) && (g_sChar.m_cLocation.Y - Enemy.m_cLocation.Y <= 5))))
-	{ //Checks for player vertically
+	if ((Enemy.m_cLocation.X == g_sChar.m_cLocation.X) && //Checks for player vertically
+		(((Enemy.m_cLocation.Y > g_sChar.m_cLocation.Y) && (Enemy.m_cLocation.Y - g_sChar.m_cLocation.Y <= 10)) || //Checking upwards
+		((g_sChar.m_cLocation.Y > Enemy.m_cLocation.Y) && (g_sChar.m_cLocation.Y - Enemy.m_cLocation.Y <= 10)))) //Checking downwards
+	{ 
 		detected = true;
 	}
-	else if ((Enemy.m_cLocation.Y == g_sChar.m_cLocation.Y) && (((Enemy.m_cLocation.X > g_sChar.m_cLocation.X) && (Enemy.m_cLocation.X - g_sChar.m_cLocation.X <= 5)) || ((g_sChar.m_cLocation.X > Enemy.m_cLocation.X) && (g_sChar.m_cLocation.X - Enemy.m_cLocation.X <= 5))))
-	{ //Checks for player horizontally
+	else if ((Enemy.m_cLocation.Y == g_sChar.m_cLocation.Y) && //Checks for player horizontally
+		(((Enemy.m_cLocation.X > g_sChar.m_cLocation.X) && (Enemy.m_cLocation.X - g_sChar.m_cLocation.X <= 10)) || //Checking left
+		((g_sChar.m_cLocation.X > Enemy.m_cLocation.X) && (g_sChar.m_cLocation.X - Enemy.m_cLocation.X <= 10)))) //Checking right
+	{ 
 		detected = true;
 	}
 
@@ -24,25 +28,33 @@ void AiMovement()
 	{
 		randomMovement = rand() % 4 + 1;
 		bool bSomethingHappened = false;
-		if (g_dBounceTime > g_dElapsedTime)
+		if (enemyMoveTime > g_dElapsedTime)
 			return;
 
-		if ((randomMovement == 1) && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '|') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'T'))
+		if ((randomMovement == 1) && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '|') && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'T'))
 		{ //Enemy moves up if character isn't detected
 			Enemy.m_cLocation.Y--;
 			bSomethingHappened = true;
 		}
-		else if ((randomMovement == 2) && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '|') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'T'))
+		else if ((randomMovement == 2) && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '|') && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'T'))
 		{ //Enemy moves down if character isn't detected
 			Enemy.m_cLocation.Y++;
 			bSomethingHappened = true;
 		}
-		else if ((randomMovement == 3) && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '|') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'T'))
+		else if ((randomMovement == 3) && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '|') && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'T'))
 		{ //Enemy moves left if character isn't detected
 			Enemy.m_cLocation.X--;
 			bSomethingHappened = true;
 		}
-		else if ((randomMovement == 4) && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '|') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'T'))
+		else if ((randomMovement == 4) && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '|') && 
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'T'))
 		{ //Enemy moves right if character isn't detected
 			Enemy.m_cLocation.X++;
 			bSomethingHappened = true;
@@ -50,13 +62,54 @@ void AiMovement()
 
 		if (bSomethingHappened)
 		{
-			g_dBounceTime = g_dElapsedTime + 0.2;
+			enemyMoveTime = g_dElapsedTime + 0.125;
 		}
 	}
-	/*else
+	else if (detected == true)
 	{
+		bool bSomethingHappened = false;
+		if (enemyMoveTime > g_dElapsedTime)
+			return;
 
-	}*/
+		if ((g_sChar.m_cLocation.X == Enemy.m_cLocation.X) && (Enemy.m_cLocation.Y > g_sChar.m_cLocation.Y) && (Enemy.m_cLocation.Y - g_sChar.m_cLocation.Y <= 10) && //Enemy moves up if character is detected upwards
+			(Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != '|') && //Collision
+			(Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y - 1][Enemy.m_cLocation.X] != 'T'))//Collision
+		{ 
+			Enemy.m_cLocation.Y--;
+			bSomethingHappened = true;
+		}
+		else if ((g_sChar.m_cLocation.X == Enemy.m_cLocation.X) && (g_sChar.m_cLocation.Y > Enemy.m_cLocation.Y) && (g_sChar.m_cLocation.Y - Enemy.m_cLocation.Y <= 10) &&
+			(Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != '|') && //Collision
+			(Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y + 1][Enemy.m_cLocation.X] != 'T'))//Collision
+		{ //Enemy moves down if character isn't detected
+			Enemy.m_cLocation.Y++;
+			bSomethingHappened = true;
+		}
+		else if ((Enemy.m_cLocation.Y == g_sChar.m_cLocation.Y) && (Enemy.m_cLocation.X > g_sChar.m_cLocation.X) && (Enemy.m_cLocation.X - g_sChar.m_cLocation.X <= 10) &&
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != '|') && //Collision
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X - 1] != 'T')) //Collision
+		{ //Enemy moves left if character isn't detected
+			Enemy.m_cLocation.X--;
+			bSomethingHappened = true;
+		}
+		else if ((Enemy.m_cLocation.Y == g_sChar.m_cLocation.Y) && (g_sChar.m_cLocation.X > Enemy.m_cLocation.X) && (g_sChar.m_cLocation.X - Enemy.m_cLocation.X <= 10) &&
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '=') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != '|') && //Collision
+			(Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'r') && (Map[LevelSelection][Enemy.m_cLocation.Y][Enemy.m_cLocation.X + 1] != 'T')) //Collision
+		{ //Enemy moves right if character isn't detected
+			Enemy.m_cLocation.X++;
+			bSomethingHappened = true;
+		}
+
+		if (bSomethingHappened)
+		{
+			enemyMoveTime = g_dElapsedTime + 0.125;
+		}
+
+		if (Enemy.m_cLocation.Y == g_sChar.m_cLocation.Y && Enemy.m_cLocation.X == g_sChar.m_cLocation.X)
+		{
+			restart();
+		}
+	}
 }
 
 void AiRender()
@@ -70,7 +123,7 @@ void AiSpawn()
 	switch (LevelSelection)
 	{
 	case 1:
-		Enemy.m_cLocation.X = 20;
+		Enemy.m_cLocation.X = 30;
 		Enemy.m_cLocation.Y = 4;
 		break;
 	}
